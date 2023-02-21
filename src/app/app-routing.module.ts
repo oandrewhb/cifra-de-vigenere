b9668662283914134dec752ea685188eb5b454aa
Andrew <andrew@kugel.com.br>
2023-02-21 13:42:23 -0300

    PRIMEIRO COMMIT - Adicionado Bootstrap 5 - Teste com rounter links para criação de SPA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'primeira-pagina', component: PrimeiraPaginaComponent},
+  {path: 'segunda-pagina', component: SegundaPaginaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuPrincipalComponent,
+    PrimeiraPaginaComponent,
+    SegundaPaginaComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
